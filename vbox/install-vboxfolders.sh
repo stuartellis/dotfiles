f2f29a870be4899616560401605142e58dff9020
Stuart Ellis <stuart@stuartellis.name>
2018-10-06 21:45:45 +0100

    Amend script for shared folders

--- vbox/install-vboxfolders.sh
+++ vbox/install-vboxfolders.sh
@@ -5,13 +5,13 @@ set -e
 SCRIPT_NAME=vboxfolders.sh
 UNIT_NAME=vboxfolders.service
 
-cp $SCRIPT_NAME /usr/local/bin
-chown root:root /usr/local/bin/$UNIT_NAME
-chmod 0770 /usr/local/bin/$SCRIPT_NAME
+cp "${SCRIPT_NAME}" /usr/local/bin
+chown root:root /usr/local/bin/"${SCRIPT_NAME}"
+chmod 0770 /usr/local/bin/"${SCRIPT_NAME}"
 
-cp $UNIT_NAME /etc/systemd/system/
-chown root:root /etc/systemd/$UNIT_NAME
-chmod 0644 /etc/systemd/$UNIT_NAME
-sudo systemctl enable $UNIT_NAME
+cp "${UNIT_NAME}" /etc/systemd/system/
+chown root:root /etc/systemd/"${UNIT_NAME}"
+chmod 0644 /etc/systemd/"${UNIT_NAME}"
+sudo systemctl enable "${UNIT_NAME}"
 
 sudo systemctl daemon-reload
