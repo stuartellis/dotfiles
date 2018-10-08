97bc22fde09e2d6abcc0cefa75880ccdf1d89675
Stuart Ellis <stuart@stuartellis.name>
2018-10-08 20:54:18 +0100

    Add directory for Fedora

--- fedora/setup.sh
+++ fedora/setup.sh
@@ -7,39 +7,24 @@ SAVE_DIR=$HOME/dotfiles_$(date "+%Y-%m-%d-%H-%M")
 
 mkdir -p "$SAVE_DIR"
 
-# SSH directory
-SSH_DIR=$HOME/.ssh
-if [ ! -d "$SSH_DIR" ]  ; then
-  mkdir -p "$SSH_DIR"
-  chmod 0700 "$SSH_DIR"
-fi
-
-# Project directories
-PROJECTS_DIR=$HOME/Projects
-if [ ! -d "$PROJECTS_DIR" ]  ; then
-  mkdir -p "$PROJECTS_DIR"
-fi
-if [ ! -d "$PROJECTS_DIR/src" ]  ; then
-  mkdir -p "$PROJECTS_DIR/src"
-fi
-if [ ! -d "$PROJECTS_DIR/src/github.com" ]  ; then
-  mkdir -p "$PROJECTS_DIR/src/github.com"
-fi
-if [ ! -d "$PROJECTS_DIR/src/gitlab.com" ]  ; then
-  mkdir -p "$PROJECTS_DIR/src/gitlab.com"
-fi
-
 # Bash
 if [ -e "$HOME/.bashrc" ] ; then
   mv "$HOME/.bashrc" "$SAVE_DIR" 
 fi
 
-ln -s "$DOTFILES_DIR/macos/bash/.bashrc" "$HOME/.bashrc"
+ln -s "$DOTFILES_DIR/fedora/bash/.bashrc" "$HOME/.bashrc"
 
 if [ -e "$HOME/.bash_profile" ] ; then
   mv "$HOME/.bash_profile" "$SAVE_DIR" 
 fi
-ln -s "$DOTFILES_DIR/macos/bash/.bash_profile" "$HOME/.bash_profile"
+
+ln -s "$DOTFILES_DIR/fedora/bash/.bash_profile" "$HOME/.bash_profile"
+
+if [ -e "$HOME/.bash_logout" ] ; then
+  mv "$HOME/.bash_logout" "$SAVE_DIR" 
+fi
+
+ln -s "$DOTFILES_DIR/fedora/bash/.bash_logout" "$HOME/.bash_logout"
 
 # Git
 if [ -e "$HOME/.gitconfig" ] ; then
