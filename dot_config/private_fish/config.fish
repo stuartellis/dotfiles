8844d366c3b6402d7eaa869feaea7cf4b9c36559
Stuart Ellis <stuart@stuartellis.name>
2023-09-09 14:56:18 +0100

    feat: Improve config for fish shell

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,31 +1,23 @@
 
-# Rust
-if test -d '$HOME/.cargo/bin'
+# Add Rust executables to $PATH
+if test -d "$HOME/.cargo/bin"
   fish_add_path --path --append "$HOME/.cargo/bin"
 end
 
-# Local bin directory 
+# Add local bin/ directory to $PATH
 if test -d "$HOME/bin"
 	fish_add_path --path --append "$HOME/bin"
 end
 
-# Source machine-local config if it exists
-
-set --local FISH_CONFIG_ROOT (dirname (status --current-filename))
-if test -f "$FISH_CONFIG_ROOT/config.local.fish"
-  source "$FISH_CONFIG_ROOT/config.local.fish"
-end
-
 if status is-interactive
 
-    # Use VS Code as editor if available
+    # Use Visual Studio Code as editor if available
     set -gx EDITOR vim
     command -q code && set -gx EDITOR code
 
-    # Add abbr to call recipes in user Justfile by typing "j"
+    # Add abbr to call recipes in user Justfile by typing ".j"
     if test -x '/usr/bin/just'
-	    abbr --add j just --justfile ~/.user.justfile --working-directory .
-	  end
+      abbr --add .j just --justfile ~/.user.justfile --working-directory .
+    end
 
 end
-
